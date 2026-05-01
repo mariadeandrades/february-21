c1a6d0a642bd380c4262a209c11e74bb590bd50f
Maria de Andrade <mariaclaradeandradesilva11@gmail.com>
2026-05-01 13:13:08 -0300

    studying recursion

---
+++ Go files/learning recursion/ex05.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt" 
+
+func array(arr []int ) int{
+	if len(arr) == 0{
+		return 0
+	}
+	return arr[0] + array(arr[1:]) //soma o número com o próximo
+}
+
+func main() {
+	var n int
+	fmt.Println("Quantos números você deseja somar?")
+	fmt.Scan(&n)
+	for n <= 0{
+		fmt.Println("A quantidade de números deve ser um valor positivo não-nulo:")
+		fmt.Scan(&n)
+	}
+	arr := make([]int, n)
+	fmt.Println("Digite, a seguir, os", n,"números:")
+	for i := 0; i < n; i ++{
+		fmt.Scan(&arr[i])
+	}
+	soma := array(arr)
+	fmt.Printf("A soma é %v.", soma)
+}
