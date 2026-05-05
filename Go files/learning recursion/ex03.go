09740834ec6976cb5411da367cc110702f915d19
Maria de Andrade <mariaclaradeandradesilva11@gmail.com>
2026-05-04 21:32:57 -0300

    more stuff

--- Go files/learning recursion/ex03.go
+++ Go files/learning recursion/ex03.go
@@ -3,12 +3,15 @@ package main
 import "fmt"
 
 func fibonacci(t int) int{
+	array := make([]int, 50)
+	for i := 0; i < 50; i ++{
 	if t <= 1{
 		return t
+		array = append(array, t)
 	}
 	return fibonacci(t-1) + fibonacci(t-2)
 }
-
+}
 
 func main() {
 	var t int
